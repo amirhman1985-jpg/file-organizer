cae1087a403300ab6d89faba0384e1007392e39f
AmirHM <amirhman1985@gmail.com>
2026-08-20 21:35:42 +0330

    release: bump version to 0.3.0

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -439,7 +439,7 @@ def test_cli_version(capsys):
     captured = capsys.readouterr()
 
     assert exc_info.value.code == 0
-    assert "0.2.1" in captured.out
+    assert "0.3.0" in captured.out
 
 
 def test_cli_invalid_operation_code():
@@ -498,7 +498,7 @@ def test_free_edition_uses_free_features(capsys):
     captured = capsys.readouterr()
 
     assert (
-        "FileOrganizer Free 0.2.1"
+        "FileOrganizer Free 0.3.0"
         in captured.out
     )
 
